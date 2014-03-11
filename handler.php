cd5f8f82fe2695640bcbc29a5cb54d1f82438d5a
AJ Foster <web@aj-foster.com>
2014-03-11 01:18:14 -0400

    Fix reading of secret.txt and ignore empty file uploads.

--- handler.php
+++ handler.php
@@ -1,6 +1,6 @@
 <?php
 
-$password = file_get_contents("secret.txt");
+$password = trim(file_get_contents("secret.txt"));
 $mysqli = new mysqli("localhost", "services", $password, "jessicaluck2014");
 
 
@@ -73,42 +73,44 @@ $temp = explode(".", $_FILES["support-form-picture"]["name"]);
 $extension = end($temp);
 
 
+if ($_FILES["support-form-picture"]["name"] != "") {
 
-if ((($_FILES["support-form-picture"]["type"] == "image/gif")
-	|| ($_FILES["support-form-picture"]["type"] == "image/jpeg")
-	|| ($_FILES["support-form-picture"]["type"] == "image/jpg")
-	|| ($_FILES["support-form-picture"]["type"] == "image/pjpeg")
-	|| ($_FILES["support-form-picture"]["type"] == "image/x-png")
-	|| ($_FILES["support-form-picture"]["type"] == "image/png"))
-	&& in_array($extension, $allowedExts)) {
+	if ((($_FILES["support-form-picture"]["type"] == "image/gif")
+		|| ($_FILES["support-form-picture"]["type"] == "image/jpeg")
+		|| ($_FILES["support-form-picture"]["type"] == "image/jpg")
+		|| ($_FILES["support-form-picture"]["type"] == "image/pjpeg")
+		|| ($_FILES["support-form-picture"]["type"] == "image/x-png")
+		|| ($_FILES["support-form-picture"]["type"] == "image/png"))
+		&& in_array($extension, $allowedExts)) {
 
-	if ($_FILES["support-form-picture"]["error"] > 0) {
-		http_response_code(406);
-		die("Error: " . $_FILES["support-form-picture"]["error"]);
-	}
+		if ($_FILES["support-form-picture"]["error"] > 0) {
+			http_response_code(406);
+			die("Error: " . $_FILES["support-form-picture"]["error"]);
+		}
 
-	$newName = str_replace(" ", "-", strtolower($_POST["support-form-name"]));
-	$newName .= "." . $extension;
+		$newName = str_replace(" ", "-", strtolower($_POST["support-form-name"]));
+		$newName .= "." . $extension;
 
 
 
-	if (file_exists("uploads/" . $newName)) {
-		http_response_code(406);
-		die("Error: We already have a picture for you.<br>Your information has been saved.");
+		if (file_exists("uploads/" . $newName)) {
+			http_response_code(406);
+			die("Error: We already have a picture for you.<br>Your information has been saved.");
+		}
+
+		else {
+			if (!move_uploaded_file($_FILES["support-form-picture"]["tmp_name"],
+									"uploads/" . $newName)) {
+				http_response_code(406);
+				die("Error: Picture could not be saved.<br>Your information was recorded successfully.");
+			}
+		}
 	}
 
 	else {
-		if (!move_uploaded_file($_FILES["support-form-picture"]["tmp_name"],
-								"uploads/" . $newName)) {
-			http_response_code(406);
-			die("Error: Picture could not be saved.<br>Your information was recorded successfully.");
-		}
+		http_response_code(406);
+		die("Error: Invalid file");
 	}
-}
-
-else {
-	http_response_code(406);
-	die("Error: Invalid file");
 }
 
 echo "Thank you for your support!";
